32ac5e4e646b35afb32a859ed1147431f8ecea92
lucashm001 <lucashm001@gmail.com>
2023-10-14 17:47:41 -0300

    update Projeto Ind rev.5

---
+++ js/script_menu.js
@@ -0,0 +1,15 @@
+// Scripit responsavel pela automoção do menu hamburger
+const btnMobile = document.getElementById('btn-mobile')    //const declara a variavel, no uso do decorrer do codigo
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded'), active;
+    if(active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label, Abrir Menu');
+    }
+}
+
+btnMobile.addEventListener('click, toggleMenu')
+btnMobile;addEventListener('touchstart', toggleMenu)
