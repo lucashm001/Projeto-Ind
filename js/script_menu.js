d19b1f5d273097615effb9e7310c452321fde1eb
lucashm001 <lucashm001@gmail.com>
2023-10-21 17:45:47 -0300

    update Projeto Ind

--- js/script_menu.js
+++ js/script_menu.js
@@ -1,15 +1,17 @@
 // Scripit responsavel pela automoção do menu hamburger
-const btnMobile = document.getElementById('btn-mobile')    //const declara a variavel, no uso do decorrer do codigo
+const btnMobile = document.getElementById('btn-mobile');    //const declara a variavel, no uso do decorrer do codigo
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
     const active = nav.classList.contains('active');
-    event.currentTarget.setAttribute('aria-expanded'), active;
+    event.currentTarget.setAttribute('aria-expanded', active);
     if(active){
         event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
     } else {
-        event.currentTarget.setAttribute('aria-label, Abrir Menu');
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
     }
 }
 
-btnMobile.addEventListener('click, toggleMenu')
-btnMobile;addEventListener('touchstart', toggleMenu)
+btnMobile.addEventListener('click', toggleMenu)
+btnMobile.addEventListener('touchstart', toggleMenu)
